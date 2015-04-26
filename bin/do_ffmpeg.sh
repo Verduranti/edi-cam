38c7cb05b8b3318a15dc0cba6a0f8e7b38c8e317
Verduranti <angelofchaos@gmail.com>
2015-04-26 05:34:07 +0000

    stuff. I hate vi.

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 /home/root/bin/ffmpeg/ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video \
--b 800k -r 30 http://127.0.0.1:8082
+-b 64k -r 20 http://127.0.0.1:8082
 
+#added to aid in shutting down
+echo $! > ~/ffmpg.pid
